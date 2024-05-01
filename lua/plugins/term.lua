5801a62c2996cf22f3eabfbfd2fed6a1ae30ebeb
lvhung.dev <lvhung.dev@gmail.com>
2024-05-01 13:49:37 +0700

    Implement buffline

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -1,7 +1,22 @@
 Keymap('t', '<C-x>', '<C-\\><C-n>', MapOpts)
 
-vim.cmd('autocmd TermOpen * startinsert')
-vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')
-vim.cmd('autocmd TermClose * lua vim.api.nvim_input("<cr>")')
+-- vim.cmd('autocmd TermOpen * startinsert')
+
+vim.api.nvim_create_autocmd({ 'TermOpen' }, {
+    pattern = { '*' },
+    callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+    end
+})
+
+vim.api.nvim_create_autocmd({ 'TermClose' }, {
+    pattern = { '*' },
+    callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+        vim.api.nvim_input('<cr>')
+    end
+})
 
 return {}
